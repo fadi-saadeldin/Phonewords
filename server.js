4c8c3044af20a99b88e53eee7d550ed540c15e38
Fadi Saadeldin <fadi@Fadis-MacBook.local>
2018-09-23 18:05:48 +0300

    prepare for deployment

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use(bodyParser.json());
 //use Routes
 app.use('/api/phonewords',phonewords);
 
-
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 const port =process.env.PORT || 5000;
 
 app.listen(port,()=> console.log(`Sever running on port ${port}`));
